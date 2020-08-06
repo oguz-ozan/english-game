cb669e6a1ad4734ddc6d089c6319819b7bb863cb
oguz-ozan <oguz.ozan@ceng.deu.edu.tr>
2020-08-06 12:50:17 +0300

    word check, fetch from recents

--- game.rb
+++ game.rb
@@ -4,8 +4,11 @@ class English
   $explanation = Hash.new()
   $point = 0
   $file = "words.txt"
-  $file2 = "words2.txt"
+  $file2 = "deletedWords.txt"
+  $file3 = "falsedWords.txt"
   $deletedWords = []
+  $falsedWords = []
+
   def readFile() 
     puts "File will be read."
     File::open( "/Users/oguzozan/Desktop/ruby/english-game/#{$file}", "r" ) do |f|
@@ -39,6 +42,12 @@ class English
     end
   end
 
+  def writeFalsedFile(str)
+    File::open( "/Users/oguzozan/Desktop/ruby/english-game/#{$file3}", "a" ) do |f|
+      f << str.downcase
+    end
+  end
+
   def fillDeletedWords()
     File::open( "/Users/oguzozan/Desktop/ruby/english-game/#{$file2}", "r" ) do |f|
       file_data = f.readlines.map(&:chomp)
@@ -49,6 +58,61 @@ class English
       end
     end
   end
+
+  def fillFalsedWords()
+    File::open( "/Users/oguzozan/Desktop/ruby/english-game/#{$file3}", "r" ) do |f|
+      file_data = f.readlines.map(&:chomp)
+      if file_data.respond_to?('each')
+        file_data.each do |line|
+          $falsedWords.append(line)
+        end
+      end
+    end
+  end
+
+  def playGame(isFromRecent)
+    len = $english.length
+    $i = 0
+    $limit = 10
+    $point = 0
+
+    print "Kelime limiti girin (default:10):"
+    $limit = gets.to_i
+
+    while $i < $limit do
+      if(isFromRecent)
+        num = rand(40) + (len-40)
+        puts "num is #{num}"
+      else
+        num = rand(len)  
+      end
+      
+      puts "\nSıradaki kelime: '#{$english[num]}'\nCevap için: C. Açıklama için: E"
+      $answer = gets.chomp.downcase
+      if($answer == "e")
+        puts "Açıklama: #{$explanation["#{$english[num]}"]}"
+        puts "Cevap için: C"
+        $answer = gets.chomp.downcase
+      end
+
+      if($answer == "c")
+        puts "'#{$turkish[num]}'. Bildiysen Y, bilemediysen N"
+        $answer = gets.chomp.downcase
+        if($answer == "y")
+          $point += 1
+        else
+          writeFalsedFile("#{$english[num]}\n")
+        end 
+      end
+      $i += 1
+    end
+    puts "Skor: #{$point}/#{$limit}"
+    t = Time.now
+    File::open( "/Users/oguzozan/Desktop/ruby/english-game/results.txt", "a+" ) do |f|
+      f << "Skor: #{$point}/#{$limit}    #{t.day}/#{t.month}/#{t.year}\n"
+    end
+  end
+
 
 
 
@@ -64,11 +128,12 @@ print "1. Yeni kelime ekle\n
 3. Yakın zamanda eklediklerinden oyna\n
 4. Tüm kelimeleri göster\n
 5. Kelime sil\n
-6. Kapat\n
+6. Kelime sorgula\n
+7. Kapat\n
 Seçiminiz:"
 
 $a = gets.to_i
-break if $a == 6
+break if $a == 7
 
 # Writing to the file
 if $a == 1
@@ -79,47 +144,25 @@ if $a == 1
   puts "Açıklama (opsiyonel)"
   exp = gets
   unless ing == "" && tur == ""
-    if exp == ""
+    if($english.detect{|x| x == ing.chomp})
+      puts "HATA: Aynı kelime mevcut"
+    else
+     if exp == ""
       program.writeFile("#{ing.chomp}\t#{tur.chomp}\n")
     else
       program.writeFile("#{ing.chomp}\t#{tur.chomp}\t#{exp.chomp}\n")
     end
-  end  
+  end
+end  
 end
 
 if $a == 2
-  len = $english.length
-  $i = 0
-  $limit = 10
-  $point = 0
+  program.playGame(false)
+end
 
-  print "Kelime limiti girin (default:10):"
-  $limit = gets.to_i
-  
-  while $i < $limit do
-    num = rand(len)
-    puts "\nSıradaki kelime: '#{$english[num]}'\nCevap için: C. Açıklama için: E"
-    $answer = gets.chomp
-    if($answer == "e")
-      puts "Açıklama: #{$explanation["#{$english[num]}"]}"
-      puts "Cevap için: C"
-      $answer = gets.chomp
-    end
-  
-    if($answer == "c")
-      puts "'#{$turkish[num]}'. Bildiysen Y, bilemediysen N"
-      $answer = gets.chomp
-      if($answer == "y")
-      $point += 1
-      end 
-    end
-    $i += 1
-  end
-  puts "Skor: #{$point}/#{$limit}"
-  t = Time.now
-   File::open( "/Users/oguzozan/Desktop/ruby/english-game/results.txt", "a+" ) do |f|
-      f << "Skor: #{$point}/#{$limit}    #{t.day}/#{t.month}/#{t.year}\n"
-    end
+
+if $a == 3
+  program.playGame(true)
 end
 
 if $a == 4
@@ -144,6 +187,13 @@ if $a == 5
 end
 
 if $a == 6
+  puts "Aradığınız kelimeyi girin:"
+  word = gets.chomp.downcase
+  if $english.detect {|x| x == word}
+    puts "Türkçesi: #{$turkish[$english.index(word)]}\nAçıklama: #{$explanation[word]}\n"
+  else
+    puts "Kelime bulunamadı.\n"
+  end
   end
 
 
